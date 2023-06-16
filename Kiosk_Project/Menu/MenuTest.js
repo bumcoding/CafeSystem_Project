4cd5ad4c7a7b99c088b806d373f34024273bf42a
Park-Bumchan <bumchan123@naver.com>
2023-06-16 18:10:14 +0900

    메뉴 다시 수정

--- Kiosk_Project/Menu/MenuTest.js
+++ Kiosk_Project/Menu/MenuTest.js
@@ -1,10 +1,10 @@
 // 메뉴 폴더들 안의 파일을 불러와 정상적으로 잘 작동하는지 테스트하는 파일
 
-import CafeMenu from './CafeMenu.js';
 import AdeMenu from './Factories/AdeMenu.js';
+import CoffeeMenu from './Factories/CoffeeMenu.js';
 import LatteMenu from './Factories/LatteMenu.js';
 
-const customer1 = new CafeMenu();
+const customer1 = new CoffeeMenu();
 customer1.createOrder();
 console.log(customer1.getPackage());
 
